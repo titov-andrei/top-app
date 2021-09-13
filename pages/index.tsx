5ae4c803f20383dba06538ab9333172ad40646b9
titov-andrei <titov_andrei@mail.ru>
2021-09-13 11:32:31 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
-import { Htag } from "../components/Htag/Htag";
+import React from "react";
+import { Htag } from "../components";
 
 export default function Home(): JSX.Element {
-  return <div>
-    <Htag tag='h1'>Текст</Htag>
-  </div>;
+  return (
+    <div>
+      <Htag tag="h1">Текст</Htag>
+    </div>
+  );
 }

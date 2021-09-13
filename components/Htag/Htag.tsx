4b7d75577bdb4663c1f58469653dc7d08e885b51
titov-andrei <titov_andrei@mail.ru>
2021-09-13 14:11:32 +0300

    Update Htag.tsx

--- components/Htag/Htag.tsx
+++ components/Htag/Htag.tsx
@@ -1,5 +1,16 @@
 import { HtagProps } from "./Htag.props";
+import styles from './Htag.module.css';
+
 
 export const Htag = ({ tag, children }: HtagProps): JSX.Element => {
-  return <h1>{children}</h1>;
+  switch (tag) {
+    case 'h1':
+      return <h1 className={}>{children}</h1>;
+    case 'h2':
+      return <h2>{children}</h2>;
+    case 'h3':
+      return <h3>{children}</h3>;
+    default:
+      return <></>;
+  }
 };

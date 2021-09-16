39f3175e4ae422f540bae4e25464b611ac47330c
titov-andrei <titov_andrei@mail.ru>
2021-09-16 10:16:05 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Htag } from "../components";
+import { Button, Htag } from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Текст</Htag>
+      <Button appearance='primary'>Кнопка</Button>
     </>
   );
 }

9ff804238658f18b83a7bad031d94be71f0c63e6
titov-andrei <titov_andrei@mail.ru>
2021-09-16 09:22:29 +0300

    Update Button.tsx

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,10 +1,15 @@
 import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.props";
+import cn from "classnames";
 
 export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
   return (
-    <button>
-      
-    </button>
-  )
+    <button
+      className={cn(styles.button, {
+        [styles.primary]: appearance == "primary",
+        [styles.ghost]: appearance == "ghost",
+      })}
+    >
+      {children}
+    </button>);
 };
